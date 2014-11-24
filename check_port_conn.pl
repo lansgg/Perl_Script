f91fefdd354d9ca9c827f8f54337e5b9617d6165
lansgg <coffee_lanshan@sina.com>
2014-11-24 17:34:39 +0800

    add check_port_conn.pl

---
+++ check_port_conn.pl
@@ -0,0 +1,121 @@
+#!/usr/bin/perl
+##Author : Aries
+##
+use strict;
+use warnings;
+
+use Net::SNMP;
+
+## ============================================================
+
+my $BK_OK = 0;
+my $BK_WARNING = 1;
+my $BK_CRITICAL = 2;
+my $BK_UNKOWN = 3;
+
+## ============================================================
+
+my $host_name = "";
+my $community = "public";
+my $warning = 20;
+my $critical = 25;
+my $port = "";
+
+while (defined(my $arg = shift)) {
+    if ($arg eq "-c") {
+        $critical = shift;
+        next;
+    }elsif ($arg eq "-w") {
+        $warning = shift;
+        next;
+    }elsif ($arg eq "-C") {
+        $community = shift;
+        next;
+    }elsif ($arg eq "-H") {
+        $host_name = shift;
+        next;
+    }elsif ($arg eq "-h") {
+        &print_help();
+        exit $BK_CRITICAL;
+    }elsif ($arg eq "-p") {
+        $port = shift;
+        next;
+    }else {
+        next;
+    }
+}
+
+if ($host_name eq "") {
+    print "Host missed!\n";
+	print_help();
+    exit $BK_UNKOWN;
+}
+
+if ($port eq "") {
+    print "port missed!\n";
+	print_help();
+    exit $BK_UNKOWN;
+}
+
+## ==============================================================
+
+my $oid_runtime = ".1.3.6.1.2.1.6.13.1.3.${host_name}.$port";
+
+print "$oid_runtime";
+
+my ($session, $error) = Net::SNMP->session(-hostname => $host_name, -community => $community, -version => 2);
+if (!defined($session)) {
+    print("UNKNOWN: SNMP Session : $error\n");
+    exit $BK_UNKOWN;
+}
+
+my $result = $session->get_table(Baseoid => $oid_runtime);
+if (!defined($result)) {
+    my $temp=$session->error;
+    $temp =~ s/'//g;
+    printf("UNKNOWN: %s.\n", $temp);
+    $session->close;
+    exit $BK_UNKOWN;
+}
+
+## ==============================================================
+
+my $count_proc = 0;
+foreach my $key (keys %$result) {
+    $count_proc++ if ($result->{$key} eq "$port");
+}
+
+$session->close;
+
+## ==============================================================
+
+if ($critical > $warning) {
+    if ($count_proc > $critical) {
+        print "Critical: Count of process $port is $count_proc\n";
+        exit $BK_CRITICAL;
+    }elsif ($count_proc >= $warning) {
+        print "Warning: Count of process $port is $count_proc\n";
+        exit $BK_WARNING;
+    }else {
+        print "OK: Count of process $port is $count_proc\n";
+        exit $BK_OK;
+    }
+}else {
+    # C <= W
+    if ($count_proc < $critical) {
+        print "Critical: Count of process $port is $count_proc\n";
+        exit $BK_CRITICAL;
+    }elsif ($count_proc < $warning) {
+        print "Warning: Count of process $port is $count_proc\n";
+        exit $BK_WARNING;
+    }else {
+        print "OK: Count of process $port is $count_proc\n";
+        exit $BK_OK;
+    }
+}
+
+## ==============================================================
+
+sub print_help {
+    print "usage:check_port_conn.pl -H [hostname or ip] -C [snmp community] -w [warning counts] -c [CRITICAL counts] -p [port]\n";
+}
